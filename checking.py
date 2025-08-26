6d139078bb39da93e887b1d328d8b0538016319c
JoNinna <148704257+JoNinna@users.noreply.github.com>
2025-08-26 08:04:39 +0400

    Cod functional

--- checking.py
+++ checking.py
@@ -11,39 +11,53 @@ load_dotenv()
 url="https://gal-dsgh.ro/leader-2023-2027/category/evenimente-de-animare/"
 
 # Termeni de cautat
-keyworks = ["Anunt ANIMARE-Iulie 2025", "Anunt ANIMARE-Septembrie 2025", "Anunt ANIMARE-Octombrie 2025"]
+keywords = ["Anunt ANIMARE-Iulie 2025", "Anunt ANIMARE-Septembrie 2025", "Anunt ANIMARE-Octombrie 2025"]
 
 # Configurație Telegram
-BOT_TOKEN = os.getenv("BOT_TOKEN")   
-CHAT_ID = os.getenv("CHAT_ID")         
+BOT_TOKEN = os.getenv("BOT_TOKEN").strip()
+CHAT_ID = os.getenv("CHAT_ID").strip()
+
+def send_telegram(message: str) -> bool:
+    # Trimite mesaj Telegram pentru raspuns API
+    if not CHAT_ID or not BOT_TOKEN:
+        print(f"[Eroare] Credentiale necorespunzatoare!")
+        return False
+    
+    urltelegram=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    try:
+        responsetelegram=requests.post(urltelegram, params={"chat_id": CHAT_ID, "text": message}, timeout=15)
+        ok = responsetelegram.status_code == 200 and responsetelegram.json().get("ok") is True
+        print("Mesajul a fost trimis pe Telegram!", responsetelegram.json())
+        return ok
+    except Exception as e:
+        print(f"Eroare la trimiterea pe Telegram: {e}")
+        return False
 
 def check_page(url: str, keywords: list[str]) -> bool:
     try:
-        response=requests.get(url)
+        response=requests.get(url, timeout=15, headers={"User-Agent": "Mozilla/5.0 Leader Check/1.0"})
+        print(f"[HTTP] GET {url} {response.status_code}, {len(response.text)} bytes")
         response.raise_for_status()
         content=response.text.lower()
 
-        for key in keyworks:
+        for key in keywords:
             if key.lower() in content:
+                print("A aparut o postare de interes pe pagina!")
                 send_telegram(f"ALERTA! A aparut rublica {key}")
                 return True
         return False
     except Exception as e:
+        print(f"Eroare la acesarea paginii")
         send_telegram(f"Eroare la acesarea paginii: {e} ")
         return False
-    
-def send_telegram(message):
-    try:
-        urltelegram=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        responsetelegram=requests.post(urltelegram, params={"chat_id": CHAT_ID, "text": message})
-        print("Mesajul a fost trimis pe Telegram!", responsetelegram.json())
-    except Exception as e:
-        print(f"Eroare la trimiterea pe Telegram: {e}")
 
-if __name__ == "__maine__":
+if __name__ == "__main__":
+    # Pentru testare:
+    # found=check_page(url, keywords)
+    # print(f"Rezultat {found}")
     while True:
         print(f"Verificare site la {datetime.now()}")
-        found=check_page()
+        found=check_page(url, keywords)
         if not found:
             print("Rublica nu a fost publicata!")
         # Site-ul este verificat o data pe zi
